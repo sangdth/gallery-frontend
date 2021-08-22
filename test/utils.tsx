4f53687365da4d19586f5b8af29ef21b1f0f0566
Sang Dang <sangdth@gmail.com>
2021-08-22 11:34:22 +0300

    export separatedly

--- test/utils.tsx
+++ test/utils.tsx
@@ -1,19 +1,19 @@
 import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
-// import { AppWrapper } from '../components'; // use this for end to end tests
+import { AppWrapper } from '../components'; // use this for end to end tests
 
 const AllTheProviders: FC = ({ children }) => {
   return (
-    <>
+    <AppWrapper>
       {children}
-    </>
+    </AppWrapper>
   );
 };
 
-const customRender = (
+const renderWithProviders = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from '@testing-library/react';
-export { customRender as render };
+export { renderWithProviders };
