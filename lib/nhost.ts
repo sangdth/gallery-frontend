b5a9eb849e7edb606b44145e3d4600bdcb17c960
Sang Dang <sangdth@gmail.com>
2022-01-21 00:28:33 +0200

    upgraded nhost v2

--- lib/nhost.ts
+++ lib/nhost.ts
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import { NhostClient } from '@nhost/nhost-js';
 import { useNhostAuth } from '@nhost/react-auth';
+import { useAtom } from 'jotai';
+import { meAtom } from '@/lib/jotai';
 import { BACKEND_ENDPOINT } from './constants';
 
 export const nhost = new NhostClient({
@@ -8,7 +11,14 @@ export const nhost = new NhostClient({
 
 export const useAuth = () => {
   const { isAuthenticated, isLoading } = useNhostAuth();
+  const [me, setMe] = useAtom(meAtom);
   const user = nhost.auth.getUser();
+
+  useEffect(() => {
+    if (!me && user) {
+      setMe(user);
+    }
+  }, [me, setMe, user]);
 
   return {
     user,
